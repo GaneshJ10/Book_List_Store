ee6a84969919b6c4c943e796a90a90e79d72935a
GANESH J <100279320+GaneshJ10@users.noreply.github.com>
2024-01-14 13:58:47 +0530

    change in config

---
+++ server/config.js
@@ -0,0 +1,4 @@
+const PORT = 5000;
+const mongoDBURL = 'mongodb+srv://GaneshJ10:Ganeshvijay10@book-store-mern.0pdhon2.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+module.exports = { PORT, mongoDBURL };
